b981d27e32f0b3d51a8916f891c9f40227c93eb3
ocif <147452915+ocif@users.noreply.github.com>
2023-11-06 01:55:38 -0500

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,1 +1,16 @@
+# FUT Web app login Infro
 
+WEB_APP_USERNAME = 'email'
+WEB_APP_PASSWORD = 'password'
+
+# Gmail logon Info
+MAILBOX_USERNAME = 'email'
+MAILBOX_PASSWORD = 'app_password' #make a apps password in gmail, 
+                                  #follow this link to set it up https://support.google.com/mail/answer/185833?hl=en, and input it here, name it FUTbot
+
+# PLayer seach info
+Player_name = 'Bukayo Saka'
+Max_buynow = '8500'
+
+# Number of this player you want to buy
+PLAYERS_TO_BUY = 1
